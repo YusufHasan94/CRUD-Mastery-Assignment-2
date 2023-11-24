8da29cff26ffe3d25eb2feedea789b8452816361
YusufHasan94 <yhpolok@gmail.com>
2023-11-24 16:57:15 +0600

    setup project

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+import mongoose from "mongoose";
+
+async function main() {
+  await mongoose.connect(process.env.DATABASE_URL as string);
+  app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
+  });
+}
+main();
